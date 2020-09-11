2242c9e8f002ffb68cd61fbe5ac7130021ca410c
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-09-11 17:35:02 +0200

    Maquetación para artículos y nuevo artículo

---
+++ js/post.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.hamburger-menu').on('click', function() {
+	    $('.bar').toggleClass('animate');
+	});
+
+	$('#btn-menu').on('click', function() {
+	    $('#menu-movil-desplegable').slideToggle();
+	});
+});
