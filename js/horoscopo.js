166ef4b0e2253b219f5410c03e43c4b2e9640c40
MSI\david <david.caballero94@hotmail.com>
2024-02-27 19:19:14 +0100

    Nuevas páginas y rutas para los horóscopos

---
+++ js/horoscopo.js
@@ -0,0 +1,26 @@
+var bubbleProductosOpen = true;
+$(document).ready(function(){
+    $('.hamburger-menu').on('click', function() {
+        $('.bar').toggleClass('animate');
+    });
+
+    $('#btn-menu').on('click', function() {
+        $('#menu-movil-desplegable').slideToggle();
+    });
+
+    $('#btn-tarot').on('click', function() {
+	    $('#menu-tarot-desplegable').slideToggle();
+	});
+
+	$('#btn-tarot-movil').on('click', function() {
+	    $('#menu-tarot-desplegable-movil').slideToggle();
+	});
+
+    $('#btn-horoscopo').on('click', function() {
+	    $('#menu-horoscopo-desplegable').slideToggle();
+	});
+
+	$('#btn-horoscopo-movil').on('click', function() {
+	    $('#menu-horoscopo-desplegable-movil').slideToggle();
+	});
+});
